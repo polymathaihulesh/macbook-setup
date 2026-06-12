dfa2510222b0b26a0cd92bf923c184826f07c766
huleshjangde <huleshjangde1@gmail.com>
2026-06-12 16:02:36 +0530

    Swap VLC for IINA

--- setup-mac.sh
+++ setup-mac.sh
@@ -31,7 +31,7 @@ sudo ln -sfn "$(brew --prefix openjdk@21)/libexec/openjdk.jdk" \
 echo "==> Step 4/8: Desktop apps"
 brew install --cask \
   kitty visual-studio-code google-chrome \
-  postman slack vlc docker mysqlworkbench burp-suite
+  postman slack iina docker mysqlworkbench burp-suite
 
 echo "==> Step 5/8: Fonts for Powerlevel10k"
 brew install --cask font-meslo-lg-nerd-font
